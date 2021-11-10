d162fbb8da328856f6dd6afee1ecd9c94607aa3d
PinkFury <jolygolf@yahoo.com>
2021-11-10 17:30:57 +0300

    Add all

---
+++ DataVault_new.sql
@@ -0,0 +1,168 @@
+﻿
+CREATE TABLE "HUB_Student" (
+    "Student_hash_key" int   NOT NULL,
+    "TaskNumber" int   NOT NULL,
+    CONSTRAINT "pk_HUB_Student" PRIMARY KEY (
+        "Student_hash_key"
+     )
+);
+
+CREATE TABLE "LINK_Tasks" (
+    "Number_hash_key" int   NOT NULL,
+    "Student_hash_key" int   NOT NULL,
+    "Task_hash_key" int   NOT NULL,
+    CONSTRAINT "pk_LINK_Tasks" PRIMARY KEY (
+        "Number_hash_key"
+     )
+);
+
+CREATE TABLE "HUB_Tasks" (
+    "Task_hash_key" int   NOT NULL,
+    "db_hash_key" int   NOT NULL,
+    "os_hash_key" int   NOT NULL,
+    "ChangedAt" date   NOT NULL,
+    CONSTRAINT "pk_HUB_Tasks" PRIMARY KEY (
+        "Task_hash_key"
+     )
+);
+
+CREATE TABLE "HUB_Databases" (
+    "Database_hash_key" int   NOT NULL,
+    CONSTRAINT "pk_HUB_Databases" PRIMARY KEY (
+        "Database_hash_key"
+     )
+);
+
+CREATE TABLE "HUB_OS" (
+    "OperatingSystem_hash_key" int   NOT NULL,
+    CONSTRAINT "pk_HUB_OS" PRIMARY KEY (
+        "OperatingSystem_hash_key"
+     )
+);
+
+CREATE TABLE "LINK_Databases" (
+    "DBlink_hash_key" int   NOT NULL,
+    "db_hash_key" int   NOT NULL,
+    "Database_hash_key" int   NOT NULL,
+    CONSTRAINT "pk_LINK_Databases" PRIMARY KEY (
+        "DBlink_hash_key"
+     )
+);
+
+CREATE TABLE "LINK_OS" (
+    "OSlink_hash_key" int   NOT NULL,
+    "os_hash_key" int   NOT NULL,
+    "OperatingSystem_hash_key" int   NOT NULL,
+    CONSTRAINT "pk_LINK_OS" PRIMARY KEY (
+        "OSlink_hash_key"
+     )
+);
+
+CREATE TABLE "SAT_Student" (
+    "Student_hash_key" int   NOT NULL,
+    "Name" string   NOT NULL,
+    "Second_Name" string   NOT NULL,
+    "Grade" int   NOT NULL,
+    CONSTRAINT "pk_SAT_Student" PRIMARY KEY (
+        "Student_hash_key"
+     )
+);
+
+CREATE TABLE "SAT_Tasks" (
+    "Task_hash_key" int   NOT NULL,
+    "Description" string   NOT NULL,
+    "CreatedAt" date   NOT NULL,
+    CONSTRAINT "pk_SAT_Tasks" PRIMARY KEY (
+        "Task_hash_key"
+     )
+);
+
+CREATE TABLE "SAT_OS" (
+    "OperatingSystem_hash_key" int   NOT NULL,
+    "Family" string   NOT NULL,
+    "Distribution" string   NOT NULL,
+    "Name" string   NOT NULL,
+    "MajorVersion" int   NOT NULL,
+    "MinorVersion" int   NOT NULL,
+    CONSTRAINT "pk_SAT_OS" PRIMARY KEY (
+        "OperatingSystem_hash_key"
+     )
+);
+
+CREATE TABLE "SAT_DB" (
+    "Database_hash_key" int   NOT NULL,
+    "Name" string   NOT NULL,
+    "Version" string   NOT NULL,
+    "Link" string   NOT NULL,
+    "CreatedAt" date   NOT NULL,
+    CONSTRAINT "pk_SAT_DB" PRIMARY KEY (
+        "Database_hash_key"
+     )
+);
+
+CREATE TABLE "SAT_TaskExtended" (
+    "Number_hash_key" int   NOT NULL,
+    "Registrator" string   NOT NULL,
+    "CreatedAt" date   NOT NULL,
+    CONSTRAINT "pk_SAT_TaskExtended" PRIMARY KEY (
+        "Number_hash_key"
+     )
+);
+
+CREATE TABLE "SAT_LicenceDB" (
+    "DBlink_hash_key" int   NOT NULL,
+    "LicenceNumber" string   NOT NULL,
+    "LicenceDate" date   NOT NULL,
+    CONSTRAINT "pk_SAT_LicenceDB" PRIMARY KEY (
+        "DBlink_hash_key"
+     )
+);
+
+CREATE TABLE "SAT_LicenceOS" (
+    "OSlink_hash_key" int   NOT NULL,
+    "LicenceNumber" string   NOT NULL,
+    "LicenceDate" date   NOT NULL,
+    CONSTRAINT "pk_SAT_LicenceOS" PRIMARY KEY (
+        "OSlink_hash_key"
+     )
+);
+
+
+ALTER TABLE "LINK_Tasks" ADD CONSTRAINT "fk_LINK_Tasks_Student_hash_key" FOREIGN KEY("Student_hash_key")
+REFERENCES "HUB_Student" ("Student_hash_key");
+
+ALTER TABLE "LINK_Tasks" ADD CONSTRAINT "fk_LINK_Tasks_Task_hash_key" FOREIGN KEY("Task_hash_key")
+REFERENCES "HUB_Tasks" ("Task_hash_key");
+
+ALTER TABLE "HUB_Tasks" ADD CONSTRAINT "fk_HUB_Tasks_db_hash_key" FOREIGN KEY("db_hash_key")
+REFERENCES "LINK_Databases" ("db_hash_key");
+
+ALTER TABLE "LINK_Databases" ADD CONSTRAINT "fk_LINK_Databases_Database_hash_key" FOREIGN KEY("Database_hash_key")
+REFERENCES "HUB_Databases" ("Database_hash_key");
+
+ALTER TABLE "LINK_OS" ADD CONSTRAINT "fk_LINK_OS_os_hash_key" FOREIGN KEY("os_hash_key")
+REFERENCES "HUB_Tasks" ("os_hash_key");
+
+ALTER TABLE "LINK_OS" ADD CONSTRAINT "fk_LINK_OS_OperatingSystem_hash_key" FOREIGN KEY("OperatingSystem_hash_key")
+REFERENCES "HUB_OS" ("OperatingSystem_hash_key");
+
+ALTER TABLE "SAT_Student" ADD CONSTRAINT "fk_SAT_Student_Student_hash_key" FOREIGN KEY("Student_hash_key")
+REFERENCES "HUB_Student" ("Student_hash_key");
+
+ALTER TABLE "SAT_Tasks" ADD CONSTRAINT "fk_SAT_Tasks_Task_hash_key" FOREIGN KEY("Task_hash_key")
+REFERENCES "HUB_Tasks" ("Task_hash_key");
+
+ALTER TABLE "SAT_OS" ADD CONSTRAINT "fk_SAT_OS_OperatingSystem_hash_key" FOREIGN KEY("OperatingSystem_hash_key")
+REFERENCES "HUB_OS" ("OperatingSystem_hash_key");
+
+ALTER TABLE "SAT_DB" ADD CONSTRAINT "fk_SAT_DB_Database_hash_key" FOREIGN KEY("Database_hash_key")
+REFERENCES "HUB_Databases" ("Database_hash_key");
+
+ALTER TABLE "SAT_TaskExtended" ADD CONSTRAINT "fk_SAT_TaskExtended_Number_hash_key" FOREIGN KEY("Number_hash_key")
+REFERENCES "LINK_Tasks" ("Number_hash_key");
+
+ALTER TABLE "SAT_LicenceDB" ADD CONSTRAINT "fk_SAT_LicenceDB_DBlink_hash_key" FOREIGN KEY("DBlink_hash_key")
+REFERENCES "LINK_Databases" ("DBlink_hash_key");
+
+ALTER TABLE "SAT_LicenceOS" ADD CONSTRAINT "fk_SAT_LicenceOS_OSlink_hash_key" FOREIGN KEY("OSlink_hash_key")
+REFERENCES "LINK_OS" ("OSlink_hash_key");
---
+++ QuickDBD-export.sql
@@ -0,0 +1,71 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "Student" (
+    "StudentID" int   NOT NULL,
+    "Name" string   NOT NULL,
+    "Second_Name" string   NOT NULL,
+    "TaskNumber" int   NOT NULL,
+    "Grade" int   NOT NULL,
+    CONSTRAINT "pk_Student" PRIMARY KEY (
+        "StudentID"
+     )
+);
+
+CREATE TABLE "TasksInNumbers" (
+    "NumberID" int   NOT NULL,
+    "NumberForStudentID" int   NOT NULL,
+    "TaskID" int   NOT NULL,
+    CONSTRAINT "pk_TasksInNumbers" PRIMARY KEY (
+        "NumberID"
+     )
+);
+
+CREATE TABLE "Tasks" (
+    "TaskID" int   NOT NULL,
+    "Description" string   NOT NULL,
+    "dbID" int   NOT NULL,
+    "osID" int   NOT NULL,
+    CONSTRAINT "pk_Tasks" PRIMARY KEY (
+        "TaskID"
+     )
+);
+
+CREATE TABLE "Databases" (
+    "DatabaseID" int   NOT NULL,
+    "Name" string   NOT NULL,
+    "Version" string   NOT NULL,
+    "Link" string   NOT NULL,
+    CONSTRAINT "pk_Databases" PRIMARY KEY (
+        "DatabaseID"
+     )
+);
+
+CREATE TABLE "OS" (
+    "OperatingSystemID" int   NOT NULL,
+    "Family" string   NOT NULL,
+    "Distribution" string   NOT NULL,
+    "Name" string   NOT NULL,
+    "MajorVersion" int   NOT NULL,
+    "MinorVersion" int   NOT NULL,
+    CONSTRAINT "pk_OS" PRIMARY KEY (
+        "OperatingSystemID"
+     )
+);
+
+ALTER TABLE "Student" ADD CONSTRAINT "fk_Student_TaskNumber" FOREIGN KEY("TaskNumber")
+REFERENCES "TasksInNumbers" ("NumberID");
+
+ALTER TABLE "TasksInNumbers" ADD CONSTRAINT "fk_TasksInNumbers_TaskID" FOREIGN KEY("TaskID")
+REFERENCES "Tasks" ("TaskID");
+
+ALTER TABLE "Tasks" ADD CONSTRAINT "fk_Tasks_dbID" FOREIGN KEY("dbID")
+REFERENCES "Databases" ("DatabaseID");
+
+ALTER TABLE "Tasks" ADD CONSTRAINT "fk_Tasks_osID" FOREIGN KEY("osID")
+REFERENCES "OS" ("OperatingSystemID");
+
